49535be952a0b3d45f5fd553183358c4136f4bda
solaa51 <5117271+solaa51@users.noreply.github.com>
2020-06-01 15:33:56 +0800

    init

---
+++ src/controller/welcome.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"fmt"
+	"gosab/system/core/myContext"
+)
+
+type Welcome struct {
+	Ctx *myContext.Context
+}
+
+func (this *Welcome) Index() {
+	this.Ctx.JsonReturn(0, "新版上线了", "")
+}
+
+func (this *Welcome) Wak() {
+	this.Ctx.JsonReturn(0, "另一个方法", "")
+}
+
+func (this *Welcome) memda() {
+	_, _ = fmt.Fprint(this.Ctx.Writer, "终于好了")
+}
